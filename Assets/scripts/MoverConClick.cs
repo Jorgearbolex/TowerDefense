e8f0f3c9f3468181b6d4f33a17c7f551fefbfda0
Jorgearbolex <jorgearbolex@gmail.com>
2019-12-10 16:20:40 -0500

    ahi va
    ahi va el tower defense

---
+++ Assets/scripts/MoverConClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoverConClick : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Camera cam;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast (cam.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        } 
+    }
+}
